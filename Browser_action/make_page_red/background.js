127480f57c49e05ffcfde79c936745c49fe578d2
صديق عبد الفتاح <  >
2017-06-29 23:59:58 +0000

    Browser_action

---
+++ Browser_action/make_page_red/background.js
@@ -0,0 +1,7 @@
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });
+});
