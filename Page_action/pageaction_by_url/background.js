8713a5722d4862b79c1a6c9f5f7ef4b829c9eb27
صديق عبد الفتاح <  >
2017-07-01 15:42:28 +0000

    Page_action

---
+++ Page_action/pageaction_by_url/background.js
@@ -0,0 +1,19 @@
+
+chrome.runtime.onInstalled.addListener(function() {
+ 
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    .
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'g' },
+          })
+        ],
+        
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
--- background.js
+++ background.js
@@ -1,7 +1,1 @@
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="blue"'
-  });
-});
+//my code 
