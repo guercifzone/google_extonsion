c104f1e00c906c994617df10c8a96613d145e059
صديق عبد الفتاح <  >
2017-07-05 15:43:28 +0100

    Override / omnibox

---
+++ omnibox/background.js
@@ -0,0 +1,15 @@
+chrome.omnibox.onInputChanged.addListener(
+  function(text, suggest) {
+    console.log('inputChanged: ' + text);
+    suggest([
+      {content: text + " one", description: "the first one"},
+      {content: text + " number two", description: "the second entry"}
+    ]);
+  });
+
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener(
+  function(text) {
+    console.log('inputEntered: ' + text);
+    alert('You just typed "' + text + '"');
+  });
